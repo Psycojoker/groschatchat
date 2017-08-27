5b45c27ad40308979dfd9bf1904dd1a4a1cb5078
Laurent Peuch <cortex@worlddomination.be>
2017-08-27 02:49:16 +0200

    [enh] sort in js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
+// stolen here https://stackoverflow.com/a/2450976
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 async function main() {
     function sleep(ms = 0) {
         return new Promise(r => setTimeout(r, ms));
     }
 
-    for (i in images) {
+    for (i in shuffle(images)) {
         $(".chat").fadeOut(function() {
             document.getElementById("tresgroschatchat").style.backgroundImage = "url(../images/" + images[i] + ")"
             document.getElementById("groschatchat").style.backgroundImage = "url(../images/" + images[i] + ")"
