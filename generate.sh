c20421be12147b249441f7dcf566729e39324a1d
Laurent Peuch <cortex@worlddomination.be>
2017-08-27 02:49:59 +0200

    [mod] shuffling is not needed anymore in bash

--- generate.sh
+++ generate.sh
@@ -5,7 +5,7 @@ touch images/empty
 
 echo "images = [" > js/images.js
 
-for image in $(ls base_images | shuf)
+for image in $(ls base_images)
 do
     gm convert base_images/$image -auto-orient -strip -interlace Line -define jpeg:preserve-settings images/$image
     echo "'$image'," >> js/images.js
